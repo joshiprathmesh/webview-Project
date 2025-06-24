19286610e678ea10bccdbd5973b2e953c59cf666
joshiprathmesh <joshiprathmesh072@gmail.com>
2025-06-24 14:28:30 +0530

    add landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
 import { LoginComponent } from "./components/login/login.component";
 import { FooterComponent } from "./components/footer/footer.component";
+import { LandingPageComponent } from "./components/landing-page/landing-page.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, LoginComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, LoginComponent, FooterComponent, LandingPageComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/components/feature-resource/feature-resource.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-feature-resource',
+  imports: [],
+  templateUrl: './feature-resource.component.html',
+  styleUrl: './feature-resource.component.css'
+})
+export class FeatureResourceComponent {
+
+}
