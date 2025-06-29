a39fc71a9c92d2ae9d0d9fee53a18bf27c569c5b
joshiprathmesh <joshiprathmesh072@gmail.com>
2025-06-29 17:07:07 +0530

    added validation in login page and added filter in All browser select button

--- src/app/components/Pages/login/login.component.ts
+++ src/app/components/Pages/login/login.component.ts
@@ -1,11 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { loginData } from '../../../../assets/loginData';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+
+  @ViewChild('form') form?:ElementRef<HTMLFormElement>;
+
+  users=loginData;
+
+  tempEmail:string="a@email.com";
+  tempPassword:string="123";
+
+  
+  onSubmit (email:string,password:string){
+    
+    // userEmail = this.users.find((user)=>user.email == email)
+    // console.log()
+
+
+    
+    // if(this.users.find((item))  && password == this.tempPassword )
+    // {
+    //   console.log("SUBMITTED....!");
+    // }
+    this.form?.nativeElement.reset();
+  }
+
 
 }
